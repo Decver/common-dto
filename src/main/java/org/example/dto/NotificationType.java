b67f8c6e61dca1c19ae9c65af230ae593b0bf7a7
Timur <32308322+Decver@users.noreply.github.com>
2025-11-10 14:04:31 +0500

    fixed the error in NotificationEvent add lib

---
+++ src/main/java/org/example/dto/NotificationType.java
@@ -0,0 +1,12 @@
+package org.example.dto;
+
+public enum NotificationType {
+    FRIEND_REQUEST,
+    FRIEND_ACCEPTED,
+    FRIEND_REMOVED,
+    POST_CREATED,
+    POST_LIKED,
+    POST_COMMENTED,
+    MESSAGE_RECEIVED,
+    SYSTEM
+}
